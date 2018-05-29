ee94dfd8f4499253c40c85276c13989ce4f21d72
fremorie <daria.smirnova.ig@gmail.com>
2018-05-29 21:44:39 +0300

    add project cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+var currentLanguage = 'en'
 
+function switchFlagIcon() {
+  var img = document.getElementById('flag-image');
+  if (currentLanguage == 'en') {
+    img.src = 'images/flags/uk.svg';
+    currentLanguage = 'ru';
+    console.log(currentLanguage);
+  } else {
+    img.src = 'images/flags/ru.svg';
+    currentLanguage = 'en'
+    console.log(currentLanguage);
+  }
+}
+
+function switchLanguage() {
+  switchFlagIcon();
+}
+
+document.onload = function() {
+  document.getElementById("flag-button").addEventListener("click", switchFlagIcon);
+}
