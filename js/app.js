da10c2dc98dd015eb3e294183060c522fd4518b5
fremorie <daria.smirnova.ig@gmail.com>
2018-06-11 01:10:25 +0300

    fill information in cv

--- js/app.js
+++ js/app.js
@@ -1,22 +1,36 @@
-var currentLanguage = 'en'
+// var currentLanguage = 'en'
+//
+// function switchFlagIcon() {
+//   var img = document.getElementById('flag-image');
+//   if (currentLanguage == 'en') {
+//     img.src = 'images/flags/uk.svg';
+//     currentLanguage = 'ru';
+//     console.log(currentLanguage);
+//   } else {
+//     img.src = 'images/flags/ru.svg';
+//     currentLanguage = 'en'
+//     console.log(currentLanguage);
+//   }
+// }
+//
+// function switchLanguage() {
+//   switchFlagIcon();
+// }
+//
+// document.onload = function() {
+//   document.getElementById("flag-button").addEventListener("click", switchFlagIcon);
+// }
 
-function switchFlagIcon() {
-  var img = document.getElementById('flag-image');
-  if (currentLanguage == 'en') {
-    img.src = 'images/flags/uk.svg';
-    currentLanguage = 'ru';
-    console.log(currentLanguage);
-  } else {
-    img.src = 'images/flags/ru.svg';
-    currentLanguage = 'en'
-    console.log(currentLanguage);
+function toggleVisibility(el) {
+  if (el.id == 'showMore') {
+    el.innerHTML = '<br>';
+    // el.style.display = 'none';
   }
-}
-
-function switchLanguage() {
-  switchFlagIcon();
-}
-
-document.onload = function() {
-  document.getElementById("flag-button").addEventListener("click", switchFlagIcon);
+  else if (el.id == 'showLess') {
+    document.getElementById('showMore').innerHTML = 'Show more';
+    // setTimeout(function() {
+    //   document.getElementById('showMore').innerHTML = 'Show more';
+    //   document.getElementById('showMore').style.display = 'inline';
+    // }, 500);
+  }
 }
