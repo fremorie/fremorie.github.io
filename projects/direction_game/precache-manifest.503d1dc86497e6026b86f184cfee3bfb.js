04db823171cb9c96bc8b045732a62b6976a2eee2
dariaborisyak <dariaborisyak@yandex-team.ru>
2020-09-14 14:23:39 +0300

    add direction_game

---
+++ projects/direction_game/precache-manifest.503d1dc86497e6026b86f184cfee3bfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e62b1e22da30f91a5cd395b0c5a4ac69",
+    "url": "/projects/direction_game/static/media/ground.e62b1e22.svg"
+  },
+  {
+    "revision": "ac42100796d9837b95780b3db1fe08ed",
+    "url": "/projects/direction_game/static/media/fish.ac421007.svg"
+  },
+  {
+    "revision": "e01b3ecf25d67c0a14a1c2dd135a68f0",
+    "url": "/projects/direction_game/static/media/algea.e01b3ecf.svg"
+  },
+  {
+    "revision": "3d1a4f8b38ce7ef6f011",
+    "url": "/projects/direction_game/static/js/runtime~main.532f4202.js"
+  },
+  {
+    "revision": "07a2178d5becaaaaf1cf",
+    "url": "/projects/direction_game/static/js/main.c37e5301.chunk.js"
+  },
+  {
+    "revision": "ee43113712d80f51329e",
+    "url": "/projects/direction_game/static/js/2.9f621566.chunk.js"
+  },
+  {
+    "revision": "07a2178d5becaaaaf1cf",
+    "url": "/projects/direction_game/static/css/main.46ce2098.chunk.css"
+  },
+  {
+    "revision": "51e010df3f735baae5d9bdc9b77a265e",
+    "url": "/projects/direction_game/index.html"
+  }
+];
